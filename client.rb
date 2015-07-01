c3b21610fc177e7625bffda29b093eadfdaef3a9
angela4713 <ang3l_gu@hotmail.com>
2015-07-01 17:08:14 -0500

    Add gems list

--- client.rb
+++ client.rb
@@ -12,5 +12,15 @@ class Client
 		connection = Faraday.new "https://rubygems.org/api/v1/api_key.json"
 		connection.basic_auth(@user, @password)
 		response = connection.get
+		response_parse = JSON.parse(response.body)
+		api_key = response_parse["rubygems_api_key"]
+	end
+
+	def gems(token)
+		connection = Faraday.new 
+		response = connection.get 'https://rubygems.org/api/v1/gems.json', {}, {'Authorization' => token}
+		response_parse = JSON.parse(response.body)
 	end
 end
+
+#c7bbbf77186860fcdc3199eb6071ea2d
