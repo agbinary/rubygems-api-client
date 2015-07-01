7726524423a3585369d6cc9c3afb67fdd9b9dca2
angela4713 <ang3l_gu@hotmail.com>
2015-07-01 17:15:16 -0500

    Indentation fix

--- client.rb
+++ client.rb
@@ -3,24 +3,22 @@ require 'json'
 
 class Client
 
-	def initialize(user, password)
-		@user = user
-		@password = password
-	end
+  def initialize(user, password)
+	@user = user
+	@password = password
+  end
 
-	def authenticate
-		connection = Faraday.new "https://rubygems.org/api/v1/api_key.json"
-		connection.basic_auth(@user, @password)
-		response = connection.get
-		response_parse = JSON.parse(response.body)
-		api_key = response_parse["rubygems_api_key"]
-	end
+  def authenticate
+	connection = Faraday.new "https://rubygems.org/api/v1/api_key.json"
+	connection.basic_auth(@user, @password)
+	response = connection.get
+	response_parse = JSON.parse(response.body)
+	api_key = response_parse["rubygems_api_key"]
+  end
 
-	def gems(token)
-		connection = Faraday.new 
-		response = connection.get 'https://rubygems.org/api/v1/gems.json', {}, {'Authorization' => token}
-		response_parse = JSON.parse(response.body)
-	end
+  def gems(token)
+	connection = Faraday.new 
+	response = connection.get 'https://rubygems.org/api/v1/gems.json', {}, {'Authorization' => token}
+	gems_owner = JSON.parse(response.body)
+  end
 end
-
-#c7bbbf77186860fcdc3199eb6071ea2d
