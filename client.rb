c0c8da6b70092c7ddd756429e2601416242d681a
angela4713 <ang3l_gu@hotmail.com>
2015-07-02 15:41:30 -0500

    Code refactoring

--- client.rb
+++ client.rb
@@ -1,24 +1,21 @@
 require 'faraday'
 require 'json'
 
-class Client
-
-  def initialize(user, password)
-    @user = user
-    @password = password
-  end
-
-  def authenticate
-    connection = Faraday.new "https://rubygems.org/api/v1/api_key.json"
-    connection.basic_auth(@user, @password)
+# Module Client for authenticate and return gems
+module Client
+  def authenticate(user, password)
+    connection = Faraday.new 'https://rubygems.org/api/v1/api_key.json'
+    connection.basic_auth(user, password)
     response = connection.get
-    response_parse = JSON.parse(response.body)
-    api_key = response_parse["rubygems_api_key"]
+    JSON.parse(response.body)['rubygems_api_key']
   end
 
   def gems(token)
-    connection = Faraday.new 
-    response = connection.get 'https://rubygems.org/api/v1/gems.json', {}, {'Authorization' => token}
-    gems_owner = JSON.parse(response.body)
+    connection = Faraday.new
+    response = connection.get do |req|
+      req.url 'https://rubygems.org/api/v1/gems.json'
+      req.headers = { 'Authorization' => token   }
+    end
+    JSON.parse(response.body)
   end
 end
